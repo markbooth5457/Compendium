f658fc42f47f7bd7212e3c8ef3a75ca99ac2d3aa
Mark Booth <mark@markandpol.com>
2019-11-24 13:02:48 +0000

    Initial Commit

---
+++ Compendium WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  Compendium WatchKit Extension
+//
+//  Created by Mark Booth on 24/11/2019.
+//  Copyright © 2019 Mark Booth. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
